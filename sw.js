987869276aaa46cc42ce1b58b3fa18c2b1d3e6a5
py6ch5zrzf-svg <py6ch5zrzf@privaterelay.appleid.com>
2026-09-01 23:18:00 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='moe-raspisanie-8-9';
+const CACHE='moe-raspisanie-9-0';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
