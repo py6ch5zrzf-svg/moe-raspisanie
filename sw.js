6421e718a6214b0ba7d2a1e201e3cecc383a5b22
py6ch5zrzf-svg <py6ch5zrzf@privaterelay.appleid.com>
2026-09-02 11:40:10 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-const CACHE='moe-raspisanie-9-0';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='moe-raspisanie-9-4';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',event=>event.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+));
+self.addEventListener('activate',event=>event.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k.startsWith('moe-raspisanie-')&&k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin) return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
